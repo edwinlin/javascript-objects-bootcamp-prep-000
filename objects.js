2cf93ed6f9ef5749e7283efc3a52978ff6d13840
Edwin Lin <edwinlin@gmail.com>
2018-11-03 15:52:54 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,8 +0,0 @@
-function updateObjectWithKeyAndValue(obj, key, value) {
- 
-  return Object.assign({}, obj, { [key]: value })
-}
-const recipe = { eggs: 3 }
- 
-updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
-console.log(recipe)
